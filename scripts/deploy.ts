281dffd5d2f4e9854c900ee9bdcee1f46dae6caf
minniejung <jungmignonne@gmail.com>
2025-04-29 14:46:30 +0900

    solved

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,26 @@
-import { ethers } from 'hardhat';
-import { makeAbi } from './makeABI';
+import { ethers } from "hardhat";
+import { makeAbi } from "./makeABI";
 
 async function main() {
-  const Proxy = await ethers.getContractFactory('Proxy');
-  const V1 = await ethers.getContractFactory('V1');
+  const Proxy = await ethers.getContractFactory("Proxy");
+  const V1 = await ethers.getContractFactory("V1");
 
-  console.log('Deploying Contract...');
+  console.log("Deploying Contract...");
 
   // Todo: 아래에 Proxy 컨트랙트와 V1 컨트랙트가 배포될 수 있도록 script를 완성시켜 주세요.
-  const v1;
 
-  const proxy;
+  const v1 = await V1.deploy();
+  await v1.waitForDeployment();
+
+  const proxy = await Proxy.deploy(v1.target);
+  await proxy.waitForDeployment();
 
   /* setting */
-  console.log('Contract deployed to:', proxy.target);
-  await makeAbi('Proxy', `${proxy.target}`);
+  console.log("Contract deployed to:", proxy.target);
+  await makeAbi("Proxy", `${proxy.target}`);
 
-  console.log('\nContract deployed to:', v1.target);
-  await makeAbi('V1', `${proxy.target}`);
+  console.log("\nContract deployed to:", v1.target);
+  await makeAbi("V1", `${proxy.target}`);
 }
 
 main()
--- scripts/upgrade.ts
+++ scripts/upgrade.ts
@@ -1,15 +1,15 @@
-import { ethers as hre } from 'hardhat'; // hardhat을 사용할 때 hre 라는 이름으로 사용합니다.
-import { makeAbi } from './makeABI';
-import { ethers } from 'ethers';
-import { admin } from '../helper/test.helper';
+import { ethers as hre } from "hardhat"; // hardhat을 사용할 때 hre 라는 이름으로 사용합니다.
+import { makeAbi } from "./makeABI";
+import { ethers } from "ethers";
+import { admin } from "../helper/test.helper";
 
-import Proxy from '../abis/Proxy.json';
+import Proxy from "../abis/Proxy.json";
 
 async function main() {
   const proxy = new ethers.Contract(Proxy.address, Proxy.abi, admin);
-  const V2 = await hre.getContractFactory('V2');
+  const V2 = await hre.getContractFactory("V2");
 
-  console.log('Deploying Contract...');
+  console.log("Deploying Contract...");
 
   /*
     Todo: 배포 스크립트(hardhat 사용)와 업그레이드(ethers 사용)를 완성시켜 주세요.
@@ -17,11 +17,17 @@ async function main() {
     1) V2 컨트랙트를 배포하기 위한 스크립트를 완성 시킵니다.
     2) 업그레이드를 실행합니다. Proxy 컨트랙트에서 upgrade 함수를 실행시켜야 합니다. 
   */
-  const v2;
+  const v2 = await V2.deploy();
+  await v2.waitForDeployment();
+
+  const upgradeTx = await proxy.upgrade(v2.target);
+  await upgradeTx.wait();
+
+  console.log("Proxy successfully upgraded to V2!");
 
   /* setting */
-  console.log('Contract deployed to:', v2.target);
-  await makeAbi('V2', `${proxy.target}`);
+  console.log("Contract deployed to:", v2.target);
+  await makeAbi("V2", `${proxy.target}`);
 }
 
 main()
